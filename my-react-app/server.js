0ca12b3555576a5d48c320a1993b471c5381c063
Apiksha <panditaapiksha@gmail.com>
2025-06-16 16:17:30 +0530

    minor update

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.get('/search', async (req, res) => {
