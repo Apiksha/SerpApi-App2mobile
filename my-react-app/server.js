fc3331ac601cdff888410f4e6dd923853ecbe67d
Apiksha <panditaapiksha@gmail.com>
2025-06-16 16:03:57 +0530

    minor update

--- my-react-app/server.js
+++ my-react-app/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/search', async (req, res) => {
   const query = req.query.q;
